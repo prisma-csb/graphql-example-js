748dad361dd3406a305cef938b94ec7ab2c06acd
Nikolas Burk <nikolas.burk@gmail.com>
2019-01-07 17:26:07 +0100

    sync with graphql example

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,71 +1,34 @@
-const { prisma } = require("../src/generated/prisma-client");
+// // THIS SCRIPT WILL BE USED FOR SEEDING WHEN THIS GITHUB ISSUE IS FIXED:
+// // https://github.com/prisma/prisma/issues/3596
+//
+// const { prisma } = require('../src/generated/prisma-client')
+//
+// async function main() {
+//   await prisma.createUser({
+//     email: "alice@prisma.io",
+//     name: "Alice",
+//     posts: {
+//       create: {
+//         title: "Join us for GraphQL Conf 2019 in Berlin",
+//         content: "https://www.graphqlconf.org/",
+//         published: true
+//       }
+//     }
+//   })
+//   await prisma.createUser({
+//     email: "bob@prisma.io",
+//     name: "Bob",
+//     posts: {
+//       create: [{
+//         title: "Subscribe to GraphQL Weekly for community news",
+//         content: "https://graphqlweekly.com/",
+//         published: true
+//       }, {
+//         title: "Follow Prisma on Twitter",
+//         content: "https://twitter.com/prisma",
+//       }]
+//     }
+//   })
+// }
 
-async function run() {
-  const ada = await prisma.createUser({
-    name: "Ada",
-    email: "ada@prisma.io",
-    posts: {
-      create: [
-        {
-          title: "Introducing the Analytical Engine",
-          content:
-            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea."
-        },
-        {
-          title: "Building General-Purpose Computers",
-          published: true,
-          content:
-            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea."
-        },
-        {
-          title: "Why Algorithms are Awesome",
-          published: true,
-          content:
-            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea."
-        }
-      ]
-    }
-  });
-
-  const grace = await prisma.createUser({
-    name: "Grace",
-    email: "grace@prisma.io",
-    posts: {
-      create: [
-        {
-          title: "Five Things You Didn't Know About Compilers",
-          content:
-            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea."
-        },
-        {
-          title: "Progamming with English Words",
-          published: true,
-          content:
-            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea."
-        }
-      ]
-    }
-  });
-
-  const lynn = await prisma.createUser({
-    name: "Lynn",
-    email: "lynn@prisma.io",
-    posts: {
-      create: [
-        {
-          title: "Working at Xerox PARC",
-          content:
-            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea."
-        },
-        {
-          title: "Introduction to VLSI Systems",
-          published: true,
-          content:
-            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea."
-        }
-      ]
-    }
-  });
-}
-
-run().catch(e => console.error(e));
+// main().catch(e => console.error(e))
