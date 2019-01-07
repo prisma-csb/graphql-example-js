53faa2ba46ba0728b8b7dca59dbfd66164d73322
Nikolas Burk <nikolas.burk@gmail.com>
2019-01-07 15:43:47 +0100

    initial commit

---
+++ src/resolvers/Query.js
@@ -0,0 +1,9 @@
+const Query = {
+  feed: (parent, args, ctx) => ctx.db.posts({ where: { published: true } }),
+  drafts: (parent, args, ctx) => ctx.db.posts({ where: { published: false } }),
+  post: (parent, { id }, ctx) => ctx.db.post({ id })
+};
+
+module.exports = {
+  Query
+};
---
+++ src/resolvers/index.js
@@ -0,0 +1,15 @@
+const { Query } = require("./Query");
+const { Mutation } = require("./Mutation");
+const { Post } = require("./Post");
+const { User } = require("./User");
+
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Post
+};
+
+module.exports = {
+  resolvers
+};
