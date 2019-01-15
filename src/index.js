2a5d527900df811b9a06f475df6f6c357db17f4d
Nikolas Burk <nikolas.burk@gmail.com>
2019-01-15 18:30:21 +0100

    add default query for playground

--- src/index.js
+++ src/index.js
@@ -71,4 +71,4 @@ const server = new GraphQLServer({
   },
 })
 
-server.start(() => console.log('Server is running on http://localhost:4000'))
+server.start({ defaultPlaygroundQuery }, () => console.log('Server is running on http://localhost:4000'))
